59e93a2b8a6a4718dad723ee8625723b8ea1f19b
HuynhLQ <huynhlq.dev@gmail.com>
2025-03-19 00:00:01 +0700

    added CrossOrigin for react-app

--- src/main/java/huynhlq/dev/udemy/firstproject/apiAction/AuthAction.java
+++ src/main/java/huynhlq/dev/udemy/firstproject/apiAction/AuthAction.java
@@ -17,8 +17,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/auth")
 public class AuthAction {
 
